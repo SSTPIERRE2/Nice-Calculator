8983f5ebb5d3611ef7cdce9bc364f9e2baefe3bf
Steve <sstpierre2@partners.org>
2018-04-05 21:11:52 -0400

    responsive button text

--- src/components/NumpadButton.js
+++ src/components/NumpadButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NumpadButton = (props) => {
 	return (
 		<div className="numpad-btn" onClick={() => props.onClick(props.display)}>
-			{props.display}
+			<span className="numpad-text">{props.display}</span>
 		</div>
 	);
 }
