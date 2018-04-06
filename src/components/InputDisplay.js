f08ad4c39c03d83dc736db693419feec5054d71e
Steve <sstpierre2@partners.org>
2018-04-05 21:13:40 -0400

    responsive input text

--- src/components/InputDisplay.js
+++ src/components/InputDisplay.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const InputDisplay = (props) => {
 	return (
 		<div className="input-display">
-			{props.input}
+			<span className="input-text">{props.input}</span>
 		</div>
 	);
 }
