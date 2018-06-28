0e14e91576f1693a8e500b9e37bb449a422776bb
sstpierre2 <stephencstpierre@gmail.com>
2018-06-28 12:08:41 -0400

    Revert "Fixed a test and added keys to numpad buttons"
    This reverts commit de404f71abe49de3a1a01969f20a3d6382ff8614.

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,14 +1,11 @@
-import React from 'react';
 import { renderNumpadButtons } from '../utils';
-import renderer from 'react-test-renderer';
 
 describe('renderNumpadButtons', () => {
 	test('it outputs something', () => {
 		const expected = [];
-		const actual = renderer.create(
-      renderNumpadButtons({ display: [ 7, 8, 9 ], onClick: () => {}, direction: 'vertical' })
-    ).toJSON();
+		const actual = renderNumpadButtons({ display: [ 7, 8, 9 ], onClick: () => {} });
 
-		expect(actual.children.length).toBe(3);
+		expect(Array.isArray(actual)).toBe(true);
+		expect(actual.length).toBe(3);
 	});
 });
